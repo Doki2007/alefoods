cbd0ea3abd9c554558e632543a9f24eae1909fe0
Jamir Garro <jamirgarroarana@hotmail.com>
2026-01-02 15:31:05 -0500

    docs: translate all text from Spanish to English

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,30 +9,30 @@ const Hero = () => {
           className="absolute inset-0 bg-cover bg-center rounded-4xl"
           style={{ backgroundImage: `url(${backgroundImage.src})` }}
         >
-          <img src={backgroundImage.src} alt='Imagen de fondo.' className="w-full h-auto object-cover" />
+          <img src={backgroundImage.src} alt='Background image.' className="w-full h-auto object-cover" />
         </div>
 
         <div className="absolute inset-0 bg-black/40 rounded-4xl" />
 
         <div className="relative z-10 flex h-full items-center justify-center px-6">
           <div className="text-center text-white">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
-              Nutrición peruana que inspira confianza.
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl max-w-3xl mx-auto font-bold">
+              Peruvian nutrition that inspires your well-being.
             </h1>
             <p className="mt-4 text-base sm:text-lg md:text-xl">
               Discover the natural power of Peruvian ingredients in every
-              product we create
+              product we create.
             </p>
 
             <div className="mt-8 flex flex-col gap-4 sm:flex-row sm:justify-center">
               <button className="rounded-full bg-green-600 border-2 border-green-600 px-6 py-3 font-semibold text-black cursor-pointer transition-all ease-in-out duration-250  hover:bg-green-500 hover:border-2 hover:border-green-600 hover:text-white">
-                <a href="/productos">
-                  Ver Productos →
+                <a href="/products">
+                  View Products →
                 </a>
               </button>
               <button className="rounded-full border-2 border-white px-6 py-3 font-semibold text-white cursor-pointer transition-all ease-in-out duration-200 hover:bg-white hover:text-black">
-                <a href="/recetas">
-                  Explorar Recetas
+                <a href="/recipes">
+                  Explore Recipes
                   </a> 
               </button>
             </div>
