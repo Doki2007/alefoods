18981757a4b45251f572d9141241b7a49f0d6a47
Jamir Garro <jamirgarroarana@hotmail.com>
2026-01-25 16:43:51 -0500

    add og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alefoods.netlify.app",
+  site: "https://alefoods.vercel.app",
   output: 'static', 
   integrations: [
     sitemap({
